c756fb1f73f56a82f79079516a4c0d039e303339
Amarnath <amarnath.bagineni@costrategix.com>
2013-08-20 19:15:38 +0530

    Revert "deleting the user_guide"
    This reverts commit 8e8cf7ec17901cd9c25fd95ee5eee620ff9465a2.

--- application/models/db/DAO/baseDbClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * This class will contain all the basic functions with the database which will apply to all the 
- * tables irrespective of the modules
- * 
- * like basic fetch, insert, delete, update
- * 
- */
