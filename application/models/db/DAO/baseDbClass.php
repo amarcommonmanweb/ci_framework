8e8cf7ec17901cd9c25fd95ee5eee620ff9465a2
Amarnath <amarnath.bagineni@costrategix.com>
2013-08-20 19:15:21 +0530

    deleting the user_guide

---
+++ application/models/db/DAO/baseDbClass.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * This class will contain all the basic functions with the database which will apply to all the 
+ * tables irrespective of the modules
+ * 
+ * like basic fetch, insert, delete, update
+ * 
+ */
